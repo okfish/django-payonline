019d70e58642c57994882eda5b468a5bc6d360fd
Anikin Sergey <anikin@trilan.ru>
2013-07-10 10:47:25 +0400

    fix PayView and add backends for success end fail resquests

--- payonline/views.py
+++ payonline/views.py
@@ -1,23 +1,28 @@
 from hashlib import md5
 
-from django.http import HttpResponse, HttpResponseBadRequest
+from django.http import (HttpResponseBadRequest,
+                         HttpResponseRedirect)
 from django.shortcuts import render
 from django.utils.datastructures import SortedDict
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import View
+from django.core.urlresolvers import reverse
+
+from sitesutils.helpers import get_site
 
 from .settings import CONFIG
 from .forms import PaymentDataForm
+from payonline.loader import get_success_backends, get_fail_backends
 
 
 class PayView(View):
 
     def get_order_id(self):
-        return unicode(self.request.session.get('payonline_order_id'))
+        return unicode(self.request.session.get('payonline_order_id', ''))
 
     def get_amount(self):
-        return u'%.2f' % self.request.session.get('payonline_amount')
+        return u'%.2f' % float(self.request.session.get('payonline_amount', 0))
 
     def get_merchant_id(self):
         return CONFIG['MERCHANT_ID']
@@ -28,7 +33,18 @@ class PayView(View):
     def get_private_security_key(self):
         return CONFIG['PRIVATE_SECURITY_KEY']
 
-    def get_security_key_params(self, order):
+    def get_payonline_url(self):
+        return CONFIG['PAYONLINE_URL']
+
+    def get_callback_url(self):
+        site = get_site(self.request)
+        return 'http://%s%s' % (site.domain, reverse('payonline_callback'))
+
+    def get_fail_url(self):
+        site = get_site(self.request)
+        return 'http://%s%s' % (site.domain, reverse('payonline_fail'))
+
+    def get_security_key_params(self):
         params = SortedDict()
         params['MerchantId'] = self.get_merchant_id()
         params['OrderId'] = self.get_order_id()
@@ -41,16 +57,41 @@ class PayView(View):
         params = self.get_security_key_params()
         return md5('&'.join('='.join(i) for i in params.items())).hexdigest()
 
-    def get_context_data(self, **kwargs):
-        kwargs.update({'order_id': self.get_order_id(),
-                       'amount': self.get_amount(),
-                       'merchant_id': self.get_merchant_id(),
-                       'currency': self.get_currency(),
-                       'security_key': self.get_security_key()})
-        return kwargs
+    def get_query_params(self):
+        params = {'MerchantId': self.get_merchant_id(),
+                  'OrderId': self.get_order_id(),
+                  'Amount': self.get_amount(),
+                  'Currency': self.get_currency(),
+                  'SecurityKey': self.get_security_key(),
+                  'FailUrl': self.get_fail_url(),
+                  'ReturnUrl': self.get_callback_url()}
+        return params
+
+    def get_redirect_url(self):
+        params = self.get_query_params()
+        redirect_url = self.get_payonline_url() + '?'
+        for key, value in params.items():
+            item = '%s=%s&' % (key, value)
+            redirect_url += item
+        return redirect_url
+
+    def get_response(self, payonline_order_id, payonline_amount):
+        if payonline_order_id:
+            self.payonline_order_id = payonline_order_id
+            self.payonline_amount = payonline_amount
+            redirect_url = self.get_redirect_url()
+            return HttpResponseRedirect(redirect_url)
+        return HttpResponseBadRequest()
 
     def get(self, request, *args, **kwargs):
-        return render(request, 'payonline/pay.html', self.get_context_data())
+        payonline_order_id = self.get_order_id()
+        payonline_amount = self.get_amount()
+        if payonline_order_id:
+            self.payonline_order_id = payonline_order_id
+            self.payonline_amount = payonline_amount
+            redirect_url = self.get_redirect_url()
+            return HttpResponseRedirect(redirect_url)
+        return HttpResponseBadRequest()
 
 
 class CallbackView(View):
@@ -64,7 +105,10 @@ class CallbackView(View):
     def process_form(self, form):
         if form.is_valid():
             payment_data = form.save()
-            return HttpResponse()
+            backends = get_success_backends()
+            for backend in backends:
+                backend(payment_data)
+            return HttpResponseRedirect(reverse('payonline_success'))
         return HttpResponseBadRequest()
 
     @method_decorator(csrf_exempt)
@@ -83,6 +127,9 @@ class FailView(View):
     def post(self, request, *args, **kwargs):
         if 'ErrorCode' not in request.POST:
             return HttpResponseBadRequest()
+        backends = get_fail_backends()
+        for backend in backends:
+            backend(request.POST['ErrorCode'])
         return render(request, 'payonline/fail.html', {
             'error_code': request.POST['ErrorCode'],
         })
